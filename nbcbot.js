bb76687e2ba0efae8bb24bc776b0e128c4d25fcb
wondong <ehdgus5500@gmail.com>
2025-11-29 18:42:31 +0900

    팀배정 명령어로 팀비우기도 되도록 수정

--- nbcbot.js
+++ nbcbot.js
@@ -90,17 +90,30 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
         break;
 
       case "팀배정":
-        if(params.length <= 1) {
-          response = "파라미터가 부족합니다. (예시 : !팀배정 홍길동 블루)";
+        if(params.length <= 0) {
+          response = "파라미터가 부족합니다. (예시 : !팀배정 홍길동 블루 / !팀배정 홍길동)";
         }
 
-        paramMap = {
-          user: sender,
-          room: room,
-          isGroupChat: isGroupChat,
-          target: params.length <= 0 ? sender : params[0],
-          team: params.length <= 1 ? sender : params[1]
-        };
+        if(params.length > 1)
+        {
+          paramMap = {
+            user: sender,
+            room: room,
+            isGroupChat: isGroupChat,
+            target: params[0],
+            team: params[1],
+          };
+        }
+        else
+        {
+          paramMap = {
+            user: sender,
+            room: room,
+            isGroupChat: isGroupChat,
+            target: params[0],
+          };
+        }
+        
         response = sendRequest("/api/commands/member/team", paramMap, HttpMethod.POST);
         break;
 
@@ -248,85 +261,18 @@ function sendRequest(endpoint, paramMap, method) {
 }
 
 /**
- * POST 요청이 필요한 경우 사용
- * @param {string} endpoint - API 엔드포인트
- * @param {object} data - 전송할 데이터 객체
- * @returns {string} 서버 응답
- */
-function sendPostRequest(endpoint, data) {
-  try {
-    const urlString = SERVER_BASE_URL + endpoint;
-
-    // HTTP 요청 실행
-    const URL = java.net.URL;
-    const HttpURLConnection = java.net.HttpURLConnection;
-    const BufferedReader = java.io.BufferedReader;
-    const InputStreamReader = java.io.InputStreamReader;
-    const OutputStream = java.io.OutputStream;
-    const StringBuilder = java.lang.StringBuilder;
-
-    const url = new URL(urlString);
-    const conn = url.openConnection();
-    conn.setRequestMethod("POST");
-    conn.setConnectTimeout(REQUEST_TIMEOUT);
-    conn.setReadTimeout(REQUEST_TIMEOUT);
-    conn.setDoOutput(true);
-    conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-
-    // POST 데이터 생성
-    const postParams = [];
-    for (let key in data) {
-      postParams.push(encodeURIComponent(key) + "=" + encodeURIComponent(String(data[key])));
-    }
-    const postData = postParams.join("&");
-
-    // 데이터 전송
-    const os = conn.getOutputStream();
-    const writer = new java.io.OutputStreamWriter(os, "UTF-8");
-    writer.write(postData);
-    writer.flush();
-    writer.close();
-    os.close();
-
-    const statusCode = conn.getResponseCode();
-
-    // 응답 읽기
-    let inputStream;
-    if (statusCode >= 200 && statusCode < 300) {
-      inputStream = conn.getInputStream();
-    } else {
-      inputStream = conn.getErrorStream();
-    }
-
-    const reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-    const responseBuilder = new StringBuilder();
-    let line;
-
-    while ((line = reader.readLine()) !== null) {
-      responseBuilder.append(line);
-    }
-    reader.close();
-    conn.disconnect();
-
-    const body = responseBuilder.toString();
-
-    if (statusCode === 200) {
-      return body;
-    } else {
-      return "서버 오류: " + statusCode;
-    }
-  } catch (e) {
-    return "요청 실패: " + e.message;
-  }
-}
-
-/**
  * 도움말 메시지 반환
  */
 function getHelpMessage() {
-  return "사용 가능한 명령어:\n" +
-         "!health - 서버 상태 확인\n" +
-         "!test - 테스트 명령어\n" +
+  return "=== 사용 가능한 명령어 ===\n\n" +
+         "[서버 관리]\n" +
+         "!health - 서버 상태 확인\n\n" +
+         "[팀 관리]\n" +
+         "!팀배정 [이름] [팀] - 팀 배정\n" +
+         "  예: !팀배정 홍길동 블루\n" +
+         "!팀확인 [이름] - 팀 확인\n" +
+         "  예: !팀확인 홍길동\n\n" +
+         "[기타]\n" +
          "!echo [메시지] - 메시지 에코\n" +
          "!도움말 - 이 메시지 표시";
 }
